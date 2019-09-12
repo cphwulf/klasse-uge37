fea4e2c7725c2fdc02779a69e8a06d496bda7502
Thorbjoer Wulf <thorwulf@hotmail.com>
2019-09-12 11:32:41 +0200

    first

---
+++ NewLine.java
@@ -0,0 +1,20 @@
+public class NewLine {
+  /* 2) lav en metode der printer én linje med teksten "Hej Kurt"
+  /* 3) lav en metode der printer tre linjer med teksten "Hej"
+   * 3) lav en metode der printer én linje med en tekst som argument
+  /* 4) lav en metode der printer én linje med teksten "Hej Kurt" og returner tallet 12
+  /* 5) lav en metode der printer én linje med teksten "Hej Kurt" og returner tallet 12,22
+  /* 6) lav to metoder hvor den ene printer "Hej" og den anden "Kurt"
+  /* 7) lav to metoder hvor den ene returnerer 12 og den anden 22 
+  /* 8) lav tre metoder hvor den ene returnerer 12, den anden 22 og den tredje printer resultatet af de to første
+  /* 9) lav tre metoder hvor den ene returnerer et heltal som input fra konsollen, 
+   * den anden vurderer om tallet er mellem 10 og 20  og den tredje printer resultatet af denne test
+   * */
+  public static void main(String[] args) {
+    printLine();
+  }
+  public static void printLine() {
+    System.out.println("Hej Kurt");
+  }
+
+}
